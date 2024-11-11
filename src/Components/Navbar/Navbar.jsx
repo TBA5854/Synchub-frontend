0b25ae6ade67d8017a2239386f427d5064056407
Vikesh <vikeshkapoor100@gmail.com>
2024-11-11 17:58:28 +0530

    update - new navbar design , half of the homepage is updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,10 +8,22 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="w-full h-[10vh] flex flex-row justify-between items-center content-center text-base p-3 md:px-8 md:py-12 text-white lg:text-xl bg-transparent absolute">
-        {/* left - menu */}
+      <div className="w-full h-[10vh] flex flex-row justify-between items-center content-center text-base p-3 md:px-12 md:py-16 text-white lg:text-xl bg-transparent absolute">
+
+        {/* left - logo for desktop */}
+        <div className="md:w-[10vw] hidden md:block">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="synchub logo"
+              className="w-8 md:w-auto"
+            />
+          </Link>
+        </div>
+
+        {/* center - menu */}
         <div>
-          <ul className="hidden lg:flex flex-row justify-center items-center content-center gap-8 bg-[#7168685c] py-1 px-4 rounded-3xl backdrop-blur-sm lg:gap-10">
+          <ul className="hidden lg:flex flex-row justify-center items-center content-center gap-8 bg-[#7168685c] py-2 px-8 rounded-3xl backdrop-blur-sm lg:gap-10">
             <li>
               <NavLink
                 to="/"
@@ -124,27 +136,16 @@ const Navbar = () => {
           )}
         </div>
 
-        {/* center - logo */}
-        <div>
+        {/* center - logo for mobile*/}
+        <div className="md:hidden">
           <Link to="/">
-            <img
-              src={logo}
-              alt="synchub logo"
-              className="w-8 md:w-full relative lg:right-10"
-            />
+            <img src={logo} alt="synchub logo" className="w-8 md:w-full"/>
           </Link>
         </div>
 
-        {/* right - buttons */}
-        <div className="flex gap-4 md:text-xl">
-          <button>
-            <NavLink
-              to="/Signup"
-              className="hidden md:block px-4 py-1 bg-black rounded-3xl md:px-12"
-            >
-              Signup
-            </NavLink>
-          </button>
+
+        {/* right - login button */}
+        <div className="md:text-xl md:w-[10vw]">
           <button>
             <NavLink
               to="/Login"
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,131 +1,92 @@
-import React, { useState } from "react"
-import './Home.css';
-import img1 from "/images/img1.svg";
-import img2 from "/images/img2.svg";
-import img3 from "/images/img3.svg";
-import img4 from "/images/img4.svg";
+import React from "react";
+import invite from "../../assets/images/invite.png";
+import coding from "../../assets/images/coding.png";
+import { StretchHorizontal, Trophy, Users, Video } from "lucide-react";
+
+const cardData = [
+  {
+    icon: <Video />,
+    title: "Live video calls",
+  },
+  {
+    icon: <Trophy />,
+    title: "Leaderboard",
+  },
+  {
+    icon: <StretchHorizontal />,
+    title: "Code with your teammates",
+  },
+  {
+    icon: <Users />,
+    title: "Live AI suggestions",
+  },
+];
 
 const Home = () => {
-    const features = [
-      {
-        title: "Collaborate",
-        description:
-          "Want to collaborate on meaningful projects and make the difference",
-      },
-      {
-        title: "Innovate",
-        description:
-          "Be a part of the growing community to enhance your skills and connections",
-      },
-      {
-        title: "Succeed",
-        description:
-          "Secure your spot on the leaderboard and get a chance to win rewards",
-      },
-    ];
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {/* hero section  */}
+      <main className='md:min-h-screen min-h-[70vh] bg-[url("/images/windows11bgmob.png")] lg:bg-[url("/images/windows11bg.png")] bg-no-repeat bg-top flex flex-col justify-center items-center text-center px-2 pt-24 md:pt-48 md:px-64'>
+        <h1 className="text-5xl font-bold md:text-6xl">
+          A personalised hub for your{" "}
+          <span className="bg-gradient-to-r from-blue-600 to-cyan-600 text-transparent bg-clip-text">
+            Innovation
+          </span>
+        </h1>
+        <button className="bg-[rgb(117,60,234)] text-2xl px-20 py-2 rounded-3xl mt-16">
+          Get Started
+        </button>
+      </main>
 
-    const supportItems = [
-        {
-          title: 'Comprehensive Student Support',
-          description:
-            'SyncHub provides dedicated support to students, ensuring they have the resources and assistance needed to thrive in academic and extracurricular pursuits.',
-          img: img1,
-        },
-        {
-          title: 'Effective Project Management',
-          description: 
-            'SyncHub ensures smooth workflows and collaboration, empowering students to work together effectively on various projects and initiative',
-          img: img2,
-        },
-        {
-          title: 'Personalized Learning Experience',
-          description: 
-            "SyncHub's smart tools and resources enable students to tailor their learning journey, making education more personalized and engaging",
-          img: img3,
-        },
-        {
-          title: 'Empowering Opportunities',
-          description: 
-            "Synchub provides you oppurtunities to compete in national and International hackathon remotely with all the essential features to run the hackathon smoothly .",
-          img: img4,
-        },
-    ];
+      {/* invite section  */}
+      <section>
+        <img src={invite} alt="invite image" />
+      </section>
 
-    const [email, setEmail] = useState("");
-    const handleSubmit = () => {
-        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if (emailPattern.test(email)) {
-        console.log("Email submitted:", email);
-        setEmail("");
-        } else {
-        console.log("Email failed");
-        }
-    };
-  
-    return (
-      <div className="home">
-        <div className="center-container">
-            <h1>A personalised hub for your <span>Innovation</span></h1>
-            <button>Get Started</button>
-        </div>
-        <div className="offerings-container">
-          {/* Features Section */}
-          <div className="features">
-            {features.map((feature, index) => (
-              <div key={index} className="feature-card">
-                <h2 className="feature-title"> {feature.title} </h2>
-                <p className="feature-description">{feature.description}</p>
+      {/* features section  */}
+      <section className="min-h-screen min-w-full text-center py-32">
+        <h2 className="text-3xl md:text-5xl font-semibold">
+          FEATURES <span className="text-base">to get started with!</span>
+        </h2>
+        <div className='min-h-[50vh] min-w-full bg-[url("/images/lightBgmob.png")] md:bg-[url("/images/lightBg.png")] bg-contain bg-no-repeat mt-12 pt-4 md:pt-12'>
+          <h3 className="text-2xl md:text-4xl">LIGHT UP YOUR CODE</h3>
+          <div className="flex flex-col lg:flex-row justify-center pt-4 md:pt-20">
+            
+            {/* text area  */}
+            <div className="flex flex-col justify-between lg:px-24 ">
+              <div className="text-left p-2 md:p-0">
+                <h3 className="text-3xl md:text-5xl font-bold w-2/3 md:leading-normal">
+                  <span className="text-cyan-400">Personalised</span> coding
+                  platform for teams
+                </h3>
+                <span>
+                  SyncHub's smart tools and resources enable students to tailor
+                  their learning journey, making education more personalized and
+                  engaging.
+                </span>
               </div>
-            ))}
+              <div className="flex flex-row flex-wrap pt-10">
+                {cardData.map((card, index) => (
+                  <div
+                    key={index}
+                    className="flex-grow flex flex-col items-center gap-4 text-center p-8 hover:bg-slate-600 rounded-3xl"
+                  >
+                    <span>{card.icon}</span>
+                    <span>{card.title}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* coding image  */}
+            <div className="min-w-[50vw] flex justify-end">
+              <img src={coding} alt="coding image" />
+            </div>
           </div>
         </div>
+      </section>
+    </div>
+  );
+};
 
-        <div className="support-section">
-            {supportItems.map((item, index) => (
-                <div className={`support-item ${index % 2 === 0 ? 'row-reverse' : ''}`} key={index}>
-                <div className="support-img">
-                    <img src={item.img} alt={item.title} />
-                </div>
-                <div className="support-text">
-                    <h4>
-                    {item.title.split(' ').slice(0, -1).join(' ') + ' '}
-                    <span className="highlight">{item.title.split(' ').slice(-1)}</span>
-                    </h4>
-                    <p>{item.description}</p>
-                </div>
-                
-                <div className="divider"></div>
-                </div>
-            ))}
-        </div>
-
-        <div className="container">
-            <div className="content">
-                <div className="headline">
-                    Elevate your student experience with SyncHub's cutting-edge solutions designed to maximize productivity and collaboration.
-                </div>
-
-                <div className="input-container">
-                    <button
-                        onClick={handleSubmit}
-                        className="submit-button"
-                    >
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="email-input"
-                        />
-                        Submit
-                    </button>
-                </div>
-                <p className="connect-message">Connect with Us to know more!</p>
-            </div>
-        </div>
-      </div>
-    );
-  };
-  
-  export default Home;
+export default Home;
