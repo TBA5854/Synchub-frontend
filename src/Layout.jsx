368a9c5f43f777fa42890283b2705c4ddd0fa3dc
Vikesh <vikeshkapoor100@gmail.com>
2024-12-15 21:17:04 +0530

    fix : import issue

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import {Outlet} from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
 import ScrollToTop from './Components/ScrollToTop/ScrollToTop'
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "./Components/ui/toaster"
 const Layout = () => {
   return (
     <>
